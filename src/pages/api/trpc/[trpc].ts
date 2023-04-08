6e73b0beaada3af6be04a71fcb96af92b3036944
Kenny Tran <82717216+ktra99@users.noreply.github.com>
2023-04-08 09:25:59 +0200

    add context

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,9 @@
+import { createContext } from "@src/server/context";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { appRouter } from "../../../server/routers/_app";
 // export API handler
 // @see https://trpc.io/docs/api-handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
-  createContext: () => ({}),
+  createContext,
 });
