b6adc2ca1034f2fb742837849a658fba0809ff38
Kenny Tran <82717216+ktra99@users.noreply.github.com>
2023-04-10 00:26:46 +0200

    try parsing body

--- src/pages/api/clerk/webhook.ts
+++ src/pages/api/clerk/webhook.ts
@@ -7,19 +7,25 @@ export default async function handler(
   res: NextApiResponse<string>
 ) {
   try {
-    const { body, headers } = req;
+    const { headers } = req;
+    const raw = (await buffer(req)).toString();
+    const body = JSON.parse(raw);
     const svixId = headers["svix-id"]?.toString();
     const svixSignature = headers["svix-signature"]?.toString();
     const svixTimestamp = headers["svix-timestamp"]?.toString();
     if (!svixId || !svixSignature || !svixTimestamp)
       throw new Error("headers not found");
     const wh = new Webhook(process.env.CLERK_SIGNIN_SECRET as string);
-    // wh.verify(body, {
-    //   "svix-id": svixId,
-    //   "svix-signature": svixSignature,
-    //   "svix-timestamp": svixTimestamp,
-    // });
-    res.status(200).send(process.env.CLERK_SIGNIN_SECRET as string);
+    wh.verify(body, {
+      "svix-id": svixId,
+      "svix-signature": svixSignature,
+      "svix-timestamp": svixTimestamp,
+    });
+    res
+      .status(200)
+      .send(
+        `id: ${svixId}. timestamp: ${svixTimestamp}. signature: ${svixSignature}`
+      );
   } catch (err: any) {
     res.status(500).send(err.message);
   }
