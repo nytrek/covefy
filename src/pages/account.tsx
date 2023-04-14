2b40055f98bd52f15c57147f33f117e94341cc7e
Kenny Tran <82717216+ktra99@users.noreply.github.com>
2023-04-14 17:10:06 +0200

    add safeguards

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -35,84 +35,86 @@ export default function Account() {
   return (
     <>
       <Navbar />
-      <main className="pb-36 pt-12">
-        <div className="mx-auto max-w-3xl space-y-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="md:flex md:items-center md:justify-between md:space-x-5">
-            <div className="flex items-center space-x-5">
-              <div className="flex-shrink-0">
-                <div className="relative">
-                  {profile.data?.imageUrl ? (
-                    <img
-                      className="h-16 w-16 rounded-full"
-                      src={profile.data?.imageUrl}
-                      alt=""
+      {profile.data ? (
+        <main className="pb-36 pt-12">
+          <div className="mx-auto max-w-3xl space-y-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="md:flex md:items-center md:justify-between md:space-x-5">
+              <div className="flex items-center space-x-5">
+                <div className="flex-shrink-0">
+                  <div className="relative">
+                    {profile.data?.imageUrl ? (
+                      <img
+                        className="h-16 w-16 rounded-full"
+                        src={profile.data?.imageUrl}
+                        alt=""
+                      />
+                    ) : (
+                      <span className="block h-16 w-16 rounded-full bg-brand-700"></span>
+                    )}
+                    <span
+                      className="absolute inset-0 rounded-full shadow-inner"
+                      aria-hidden="true"
                     />
-                  ) : (
-                    <span className="block h-16 w-16 rounded-full bg-brand-700"></span>
-                  )}
-                  <span
-                    className="absolute inset-0 rounded-full shadow-inner"
-                    aria-hidden="true"
-                  />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center space-x-2">
+                    <h1 className="text-2xl font-bold text-brand-50">
+                      {profile.data.name}
+                    </h1>
+                    {profile.data.premium ? (
+                      <CheckBadgeIcon className="mt-1 h-6 w-6 text-brand-50" />
+                    ) : null}
+                  </div>
+                  <p className="text-sm font-medium text-brand-500">
+                    @{profile.data.username}
+                  </p>
                 </div>
               </div>
-              <div>
-                <div className="flex items-center space-x-2">
-                  <h1 className="text-2xl font-bold text-brand-50">
-                    {profile.data?.name}
-                  </h1>
-                  {profile.data?.premium ? (
-                    <CheckBadgeIcon className="mt-1 h-6 w-6 text-brand-50" />
-                  ) : null}
-                </div>
-                <p className="text-sm font-medium text-brand-500">
-                  @{profile.data?.username}
-                </p>
-              </div>
-            </div>
-            <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
-              <button
-                type="button"
-                className="inline-flex items-center justify-center rounded-md bg-brand-50 px-3 py-2 text-sm font-semibold text-brand-900 shadow-sm ring-1 ring-inset ring-brand-300 hover:bg-brand-50"
-              >
-                Disable account
-              </button>
-              <button
-                type="button"
-                className={clsx(
-                  !profile.data?.claim
-                    ? "cursor-not-allowed bg-brand-800 hover:bg-brand-700"
-                    : "bg-brand-600 hover:bg-brand-500",
-                  "inline-flex items-center justify-center rounded-md px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
-                )}
-                disabled={!profile.data?.claim}
-              >
-                Claim credits
-              </button>
-            </div>
-          </div>
-          <div>
-            <h3 className="text-base font-semibold leading-6 text-brand-50">
-              Your progress
-            </h3>
-            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-              {stats.map((item) => (
-                <div
-                  key={item.name}
-                  className="overflow-hidden rounded-lg border border-brand-600 bg-brand-800 px-4 py-5 shadow sm:p-6"
+              <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center rounded-md bg-brand-50 px-3 py-2 text-sm font-semibold text-brand-900 shadow-sm ring-1 ring-inset ring-brand-300 hover:bg-brand-50"
                 >
-                  <dt className="truncate text-sm font-medium text-brand-500">
-                    {item.name}
-                  </dt>
-                  <dd className="mt-1 text-3xl font-semibold tracking-tight text-brand-50">
-                    {item.stat}
-                  </dd>
-                </div>
-              ))}
-            </dl>
+                  Disable account
+                </button>
+                <button
+                  type="button"
+                  className={clsx(
+                    !profile.data.claim
+                      ? "cursor-not-allowed bg-brand-800 hover:bg-brand-700"
+                      : "bg-brand-600 hover:bg-brand-500",
+                    "inline-flex items-center justify-center rounded-md px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
+                  )}
+                  disabled={!profile.data.claim}
+                >
+                  Claim credits
+                </button>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-base font-semibold leading-6 text-brand-50">
+                Your progress
+              </h3>
+              <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                {stats.map((item) => (
+                  <div
+                    key={item.name}
+                    className="overflow-hidden rounded-lg border border-brand-600 bg-brand-800 px-4 py-5 shadow sm:p-6"
+                  >
+                    <dt className="truncate text-sm font-medium text-brand-500">
+                      {item.name}
+                    </dt>
+                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-brand-50">
+                      {item.stat}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      ) : null}
       <Footer />
     </>
   );
--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -24,7 +24,6 @@ import {
 } from "react";
 import { toast } from "react-hot-toast";
 import { Upload } from "upload-js";
-import { AnimatePresence, motion } from "framer-motion";
 
 function Modal({
   open,
@@ -386,7 +385,7 @@ function Modal({
 
 export default function Account() {
   const { user } = useUser();
-  const { query, route } = useRouter();
+  const { query } = useRouter();
   const id = query.id as string;
   const utils = trpc.useContext();
   const [open, setOpen] = useState(false);
@@ -421,12 +420,6 @@ export default function Account() {
       toast.error("API request failed, check console.log");
     },
   });
-  const tabs = [
-    { name: "My Account", href: "/account", current: route === "/account" },
-    { name: "Friend list", href: "/friends", current: route === "/friends" },
-    { name: "Billing", href: "/billing", current: route === "/billing" },
-    { name: "Support", href: "/support", current: route === "/support" },
-  ];
   const stats = [
     {
       name: "Total Likes",
@@ -474,133 +467,135 @@ export default function Account() {
     <>
       <Modal open={open} friend={profile.data} setOpen={setOpen} />
       <Navbar />
-      <main className="pb-36 pt-12">
-        <div className="mx-auto max-w-3xl space-y-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="md:flex md:items-center md:justify-between md:space-x-5">
-            <div className="flex items-center space-x-5">
-              <div className="flex-shrink-0">
-                <div className="relative">
-                  {profile.data?.imageUrl ? (
-                    <img
-                      className="h-16 w-16 rounded-full"
-                      src={profile.data?.imageUrl}
-                      alt=""
+      {profile.data ? (
+        <main className="pb-36 pt-12">
+          <div className="mx-auto max-w-3xl space-y-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="md:flex md:items-center md:justify-between md:space-x-5">
+              <div className="flex items-center space-x-5">
+                <div className="flex-shrink-0">
+                  <div className="relative">
+                    {profile.data?.imageUrl ? (
+                      <img
+                        className="h-16 w-16 rounded-full"
+                        src={profile.data?.imageUrl}
+                        alt=""
+                      />
+                    ) : (
+                      <span className="block h-16 w-16 rounded-full bg-brand-700"></span>
+                    )}
+                    <span
+                      className="absolute inset-0 rounded-full shadow-inner"
+                      aria-hidden="true"
                     />
-                  ) : (
-                    <span className="block h-16 w-16 rounded-full bg-brand-700"></span>
-                  )}
-                  <span
-                    className="absolute inset-0 rounded-full shadow-inner"
-                    aria-hidden="true"
-                  />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center space-x-2">
+                    <h1 className="text-2xl font-bold text-brand-50">
+                      {profile.data.name}
+                    </h1>
+                    {profile.data.premium ? (
+                      <CheckBadgeIcon className="mt-1 h-6 w-6 text-brand-50" />
+                    ) : null}
+                  </div>
+                  <p className="text-sm font-medium text-brand-500">
+                    @{profile.data.username}
+                  </p>
                 </div>
               </div>
-              <div>
-                <div className="flex items-center space-x-2">
-                  <h1 className="text-2xl font-bold text-brand-50">
-                    {profile.data?.name}
-                  </h1>
-                  {profile.data?.premium ? (
-                    <CheckBadgeIcon className="mt-1 h-6 w-6 text-brand-50" />
-                  ) : null}
-                </div>
-                <p className="text-sm font-medium text-brand-500">
-                  @{profile.data?.username}
-                </p>
-              </div>
-            </div>
-            <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
-              {recievingFriendStatus.data?.status === "PENDING" ||
-              sendingFriendStatus.data?.status === "ACCEPTED" ||
-              recievingFriendStatus.data?.status ===
-                "ACCEPTED" ? null : sendingFriendStatus.data?.status ===
-                "PENDING" ? (
-                <button
-                  type="button"
-                  onClick={() =>
-                    handleOnDeleteFriendRequest(
-                      sendingFriendStatus.data?.senderId,
-                      sendingFriendStatus.data?.receiverId
-                    )
-                  }
-                  className="inline-flex items-center justify-center rounded-md bg-brand-50 px-3 py-2 text-sm font-semibold text-brand-900 shadow-sm ring-1 ring-inset ring-brand-300 hover:bg-brand-50"
-                >
-                  Delete friend request
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  className="inline-flex items-center justify-center rounded-md bg-brand-50 px-3 py-2 text-sm font-semibold text-brand-900 shadow-sm ring-1 ring-inset ring-brand-300 hover:bg-brand-50"
-                >
-                  Report account
-                </button>
-              )}
+              <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
+                {recievingFriendStatus.data?.status === "PENDING" ||
+                sendingFriendStatus.data?.status === "ACCEPTED" ||
+                recievingFriendStatus.data?.status ===
+                  "ACCEPTED" ? null : sendingFriendStatus.data?.status ===
+                  "PENDING" ? (
+                  <button
+                    type="button"
+                    onClick={() =>
+                      handleOnDeleteFriendRequest(
+                        sendingFriendStatus.data?.senderId,
+                        sendingFriendStatus.data?.receiverId
+                      )
+                    }
+                    className="inline-flex items-center justify-center rounded-md bg-brand-50 px-3 py-2 text-sm font-semibold text-brand-900 shadow-sm ring-1 ring-inset ring-brand-300 hover:bg-brand-50"
+                  >
+                    Delete friend request
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="inline-flex items-center justify-center rounded-md bg-brand-50 px-3 py-2 text-sm font-semibold text-brand-900 shadow-sm ring-1 ring-inset ring-brand-300 hover:bg-brand-50"
+                  >
+                    Report account
+                  </button>
+                )}
 
-              {sendingFriendStatus.data?.status === "PENDING" ||
-              sendingFriendStatus.data?.status ===
-                "REJECTED" ? null : recievingFriendStatus.data?.status ===
-                "PENDING" ? (
-                <Link
-                  href="/friends"
-                  className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
-                >
-                  View friend request
-                </Link>
-              ) : recievingFriendStatus.data?.status === "REJECTED" ? (
-                <button
-                  type="button"
-                  onClick={() =>
-                    handleOnDeleteFriendRequest(
-                      recievingFriendStatus.data?.senderId,
-                      recievingFriendStatus.data?.receiverId
-                    )
-                  }
-                  className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
-                >
-                  Delete friend request
-                </button>
-              ) : sendingFriendStatus.data?.status === "ACCEPTED" ||
-                recievingFriendStatus.data?.status === "ACCEPTED" ? (
-                <button
-                  type="button"
-                  onClick={() => setOpen(true)}
-                  className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
-                >
-                  Send a note
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={handleOnCreateFriendRequest}
-                  className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
-                >
-                  Send friend request
-                </button>
-              )}
+                {sendingFriendStatus.data?.status === "PENDING" ||
+                sendingFriendStatus.data?.status ===
+                  "REJECTED" ? null : recievingFriendStatus.data?.status ===
+                  "PENDING" ? (
+                  <Link
+                    href="/friends"
+                    className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
+                  >
+                    View friend request
+                  </Link>
+                ) : recievingFriendStatus.data?.status === "REJECTED" ? (
+                  <button
+                    type="button"
+                    onClick={() =>
+                      handleOnDeleteFriendRequest(
+                        recievingFriendStatus.data?.senderId,
+                        recievingFriendStatus.data?.receiverId
+                      )
+                    }
+                    className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
+                  >
+                    Delete friend request
+                  </button>
+                ) : sendingFriendStatus.data?.status === "ACCEPTED" ||
+                  recievingFriendStatus.data?.status === "ACCEPTED" ? (
+                  <button
+                    type="button"
+                    onClick={() => setOpen(true)}
+                    className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
+                  >
+                    Send a note
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={handleOnCreateFriendRequest}
+                    className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
+                  >
+                    Send friend request
+                  </button>
+                )}
+              </div>
+            </div>
+            <div>
+              <h3 className="text-base font-semibold leading-6 text-brand-50">
+                {profile.data.name}&apos;s progress
+              </h3>
+              <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                {stats.map((item) => (
+                  <div
+                    key={item.name}
+                    className="overflow-hidden rounded-lg border border-brand-600 bg-brand-800 px-4 py-5 shadow sm:p-6"
+                  >
+                    <dt className="truncate text-sm font-medium text-brand-500">
+                      {item.name}
+                    </dt>
+                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-brand-50">
+                      {item.stat}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
             </div>
           </div>
-          <div>
-            <h3 className="text-base font-semibold leading-6 text-brand-50">
-              {profile.data?.name}&apos;s progress
-            </h3>
-            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-              {stats.map((item) => (
-                <div
-                  key={item.name}
-                  className="overflow-hidden rounded-lg border border-brand-600 bg-brand-800 px-4 py-5 shadow sm:p-6"
-                >
-                  <dt className="truncate text-sm font-medium text-brand-500">
-                    {item.name}
-                  </dt>
-                  <dd className="mt-1 text-3xl font-semibold tracking-tight text-brand-50">
-                    {item.stat}
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-        </div>
-      </main>
+        </main>
+      ) : null}
       <Footer />
     </>
   );
