ab5cbaccdcca429d79a8372403a67e9e7a958571
Kenny Tran <82717216+ktra99@users.noreply.github.com>
2023-04-15 12:08:14 +0200

    make dropdown panel static and use external state to configure open/close

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,11 +12,13 @@ import { trpc } from "@src/utils/trpc";
 import clsx from "clsx";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Navbar() {
   const { user } = useUser();
   const { route } = useRouter();
   const profile = trpc.getProfile.useQuery();
+  const [openPanel, setOpenPanel] = useState(false);
   const tabs = [
     { name: "My Account", href: "/account", current: route === "/account" },
     { name: "Friend list", href: "/friends", current: route === "/friends" },
@@ -61,7 +63,10 @@ export default function Navbar() {
               </div>
               <div className="flex items-center md:absolute md:inset-y-0 md:right-0 lg:hidden">
                 {/* Mobile menu button */}
-                <Popover.Button className="-mx-2 inline-flex items-center justify-center rounded-md p-2 text-brand-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-brand-50">
+                <Popover.Button
+                  onClick={() => setOpenPanel(!openPanel)}
+                  className="-mx-2 inline-flex items-center justify-center rounded-md p-2 text-brand-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-brand-50"
+                >
                   <span className="sr-only">Open menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -108,69 +113,76 @@ export default function Navbar() {
             </div>
           </div>
 
-          <Popover.Panel as="nav" className="lg:hidden" aria-label="Global">
-            <div className="pt-4">
-              <div className="mx-auto flex items-center px-4 sm:px-6">
-                <div className="flex-shrink-0">
-                  <SignedOut>
-                    <SignInButton mode="modal">
-                      <button
-                        type="button"
-                        className="flex items-center justify-center rounded-full bg-brand-600"
-                      >
-                        <UserCircleIcon className="h-10 w-10 text-brand-50" />
-                      </button>
-                    </SignInButton>
-                  </SignedOut>
+          {openPanel && (
+            <Popover.Panel
+              static
+              as="nav"
+              className="lg:hidden"
+              aria-label="Global"
+            >
+              <div className="pt-4">
+                <div className="mx-auto flex items-center px-4 sm:px-6">
+                  <div className="flex-shrink-0">
+                    <SignedOut>
+                      <SignInButton mode="modal">
+                        <button
+                          type="button"
+                          className="flex items-center justify-center rounded-full bg-brand-600"
+                        >
+                          <UserCircleIcon className="h-10 w-10 text-brand-50" />
+                        </button>
+                      </SignInButton>
+                    </SignedOut>
 
-                  <SignedIn>
-                    <UserButton />
-                  </SignedIn>
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-brand-50">
-                    {user?.fullName}
+                    <SignedIn>
+                      <UserButton />
+                    </SignedIn>
                   </div>
-                  <div className="text-sm font-medium text-brand-500">
-                    {user?.username}
+                  <div className="ml-3">
+                    <div className="text-base font-medium text-brand-50">
+                      {user?.fullName}
+                    </div>
+                    <div className="text-sm font-medium text-brand-500">
+                      {user?.username}
+                    </div>
                   </div>
+                  {user ? (
+                    <Link
+                      href="/pricing"
+                      className="ml-auto flex flex-shrink-0 items-center space-x-2 rounded-full p-1 text-brand-50"
+                    >
+                      <span className="sr-only">View notifications</span>
+                      <p>{profile.data?.credits}</p>
+                      <TicketIcon className="h-6 w-6" aria-hidden="true" />
+                    </Link>
+                  ) : null}
                 </div>
-                {user ? (
-                  <Link
-                    href="/pricing"
-                    className="ml-auto flex flex-shrink-0 items-center space-x-2 rounded-full p-1 text-brand-50"
-                  >
-                    <span className="sr-only">View notifications</span>
-                    <p>{profile.data?.credits}</p>
-                    <TicketIcon className="h-6 w-6" aria-hidden="true" />
-                  </Link>
-                ) : null}
+                <div className="mx-auto mt-3 space-y-1 px-2 sm:px-4">
+                  {tabs.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className={clsx(
+                        item.current ? "bg-brand-800" : "hover:bg-brand-800",
+                        "block rounded-md px-3 py-2 text-base font-medium text-brand-50"
+                      )}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
               </div>
-              <div className="mx-auto mt-3 space-y-1 px-2 sm:px-4">
-                {tabs.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className={clsx(
-                      item.current ? "bg-brand-800" : "hover:bg-brand-800",
-                      "block rounded-md px-3 py-2 text-base font-medium text-brand-50"
-                    )}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
 
-            <div className="mx-auto mt-6 px-4 sm:px-6">
-              <Link
-                href="/pricing"
-                className="flex w-full items-center justify-center rounded-md border border-transparent bg-brand-600 px-4 py-2 text-base font-medium text-brand-50 shadow-sm hover:bg-brand-700"
-              >
-                Go premium
-              </Link>
-            </div>
-          </Popover.Panel>
+              <div className="mx-auto mt-6 px-4 sm:px-6">
+                <Link
+                  href="/pricing"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-brand-600 px-4 py-2 text-base font-medium text-brand-50 shadow-sm hover:bg-brand-700"
+                >
+                  Go premium
+                </Link>
+              </div>
+            </Popover.Panel>
+          )}
         </>
       )}
     </Popover>
