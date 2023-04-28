b5f462fc156b6c4d7a2c3a32cbc433ecdf2a100d
Kenny Tran <82717216+ktra99@users.noreply.github.com>
2023-04-28 08:48:54 +0200

    add loading state for pinned posts

--- src/components/pinnedposts.tsx
+++ src/components/pinnedposts.tsx
@@ -31,7 +31,7 @@ export default function PinnedPosts({ handleOnUpdatePost }: Props) {
   return (
     <SignedIn>
       <AnimatePresence mode="wait">
-        {!!pinned.data && !!pinned.data.length ? (
+        {!!pinned.data ? (
           <>
             <div className="mb-8">
               <ul
@@ -39,40 +39,69 @@ export default function PinnedPosts({ handleOnUpdatePost }: Props) {
                 className="flex items-center space-x-6 overflow-x-auto"
               >
                 <AnimatePresence>
-                  {pinned.data?.map((item) => (
-                    <motion.li
-                      key={item.id}
+                  {!!!pinned.data.length ? (
+                    <motion.div
+                      key={0}
                       initial={{ opacity: 0, width: 0 }}
                       animate={{ opacity: 1, width: "auto" }}
                       exit={{ opacity: 0, width: 0 }}
                       className="relative col-span-1 flex rounded-md shadow-sm"
                     >
                       <div className="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-brand-600 text-sm font-medium text-brand-50">
-                        {item.id}
+                        # ID
                       </div>
                       <div className="flex flex-1 items-center justify-between truncate rounded-r-md bg-brand-800 pr-2">
                         <div className="flex-1 truncate px-4 py-2 text-sm">
-                          <Link
-                            href={"/post/" + item.id}
-                            className="font-medium text-brand-50"
-                          >
-                            {item.title}
-                          </Link>
+                          <h4 className="font-medium text-brand-50">
+                            Pinned notes will appear here
+                          </h4>
                           <p className="w-36 truncate text-brand-500">
-                            {item.description}
+                            Your note description
                           </p>
                         </div>
-                        <button
-                          type="button"
-                          onClick={() => handleOnUpdatePost(item, false)}
-                          className="inline-flex h-8 w-8 items-center justify-center rounded-full text-brand-500"
-                        >
-                          <span className="sr-only">Unpin</span>
-                          <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                        </button>
                       </div>
-                    </motion.li>
-                  ))}
+                    </motion.div>
+                  ) : (
+                    <>
+                      {pinned.data.map((item) => (
+                        <motion.li
+                          key={item.id}
+                          initial={{ opacity: 0, width: 0 }}
+                          animate={{ opacity: 1, width: "auto" }}
+                          exit={{ opacity: 0, width: 0 }}
+                          className="relative col-span-1 flex rounded-md shadow-sm"
+                        >
+                          <div className="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-brand-600 text-sm font-medium text-brand-50">
+                            {item.id}
+                          </div>
+                          <div className="flex flex-1 items-center justify-between truncate rounded-r-md bg-brand-800 pr-2">
+                            <div className="flex-1 truncate px-4 py-2 text-sm">
+                              <Link
+                                href={"/post/" + item.id}
+                                className="font-medium text-brand-50"
+                              >
+                                {item.title}
+                              </Link>
+                              <p className="w-36 truncate text-brand-500">
+                                {item.description}
+                              </p>
+                            </div>
+                            <button
+                              type="button"
+                              onClick={() => handleOnUpdatePost(item, false)}
+                              className="inline-flex h-8 w-8 items-center justify-center rounded-full text-brand-500"
+                            >
+                              <span className="sr-only">Unpin</span>
+                              <XMarkIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </button>
+                          </div>
+                        </motion.li>
+                      ))}
+                    </>
+                  )}
                 </AnimatePresence>
               </ul>
             </div>
@@ -88,15 +117,15 @@ export default function PinnedPosts({ handleOnUpdatePost }: Props) {
                 className="relative col-span-1 flex rounded-md shadow-sm"
               >
                 <div className="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-brand-600 text-sm font-medium text-brand-50">
-                  # ID
+                  <div className="flex h-2 w-2/5 items-center space-x-4 rounded-full bg-brand-500 motion-safe:animate-pulse"></div>
                 </div>
-                <div className="flex flex-1 items-center justify-between truncate rounded-r-md bg-brand-800 pr-2">
-                  <div className="flex-1 truncate px-4 py-2 text-sm">
+                <div className="flex flex-1 items-center justify-between truncate rounded-r-md bg-brand-800 py-3 pr-2">
+                  <div className="flex-1 space-y-2 truncate px-4 py-2 text-sm">
                     <h4 className="font-medium text-brand-50">
-                      Pinned notes will appear here
+                      <div className="flex h-2 items-center space-x-4 rounded-full bg-brand-700 motion-safe:animate-pulse"></div>
                     </h4>
                     <p className="w-36 truncate text-brand-500">
-                      Your note description
+                      <div className="flex h-2 w-1/2 items-center space-x-4 rounded-full bg-brand-700 motion-safe:animate-pulse"></div>
                     </p>
                   </div>
                 </div>
