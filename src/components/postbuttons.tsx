1017248618cb276a182af5158194af473e310208
Kenny Tran <82717216+ktra99@users.noreply.github.com>
2023-05-03 12:15:26 +0200

    add custom loading

--- src/components/postbuttons.tsx
+++ src/components/postbuttons.tsx
@@ -21,7 +21,6 @@ export default function PostButtons({
   const profile = trpc.getProfile.useQuery();
 
   const handleOnSuccess = (data: string | undefined) => {
-    toast.dismiss();
     utils.getProfile.invalidate();
     setLength((length) => data?.length ?? length);
     toast.success("Updated your post with AI generated text!");
@@ -31,7 +30,6 @@ export default function PostButtons({
   };
 
   const handleOnError = (msg: string) => {
-    toast.dismiss();
     toast.error(msg);
   };
 
@@ -42,11 +40,8 @@ export default function PostButtons({
 
   const handleOnGenerateAI = (prompt: string | undefined) => {
     if (!prompt || !profile.data) return;
-    if (profile.data.credits < 10) {
-      toast.dismiss();
+    if (profile.data.credits < 10)
       return toast.error("You don't have enough credits");
-    }
-    toast.loading("Loading...");
     generateAI.mutate({
       prompt,
       credits: profile.data.credits - 10,
@@ -59,11 +54,36 @@ export default function PostButtons({
         onClick={() => handleOnGenerateAI(descriptionRef.current?.value)}
         className="inline-flex w-full justify-center space-x-2 rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
       >
-        <span>Use AI</span>
-        <span className="flex items-center space-x-1">
-          <span>(10</span>
-          <TicketIcon className="h-5 w-5" />)
-        </span>
+        {generateAI.isLoading ? (
+          <svg
+            className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        ) : (
+          <>
+            <span>Use AI</span>
+            <span className="flex items-center space-x-1">
+              <span>(10</span>
+              <TicketIcon className="h-5 w-5" />)
+            </span>
+          </>
+        )}
       </button>
       <button
         type="submit"
