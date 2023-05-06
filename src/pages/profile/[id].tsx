06acb6bdd06a34dd5ba91b4d7c4f3fbdcda72ff2
Kenny Tran <82717216+ktra99@users.noreply.github.com>
2023-05-06 10:45:21 +0200

    change public profile buttons

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -574,14 +574,8 @@ export default function Account() {
                               href={"/board/d/" + profile.data.id}
                               className="inline-flex items-center justify-center space-x-1 rounded-md bg-brand-50 px-3 py-2 text-sm font-semibold text-brand-900 shadow-sm ring-1 ring-inset ring-brand-300 hover:bg-brand-50"
                             >
-                              Edit
+                              Visit
                             </Link>
-                            <button
-                              type="button"
-                              className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
-                            >
-                              Delete
-                            </button>
                           </div>
                         </div>
                       </div>
