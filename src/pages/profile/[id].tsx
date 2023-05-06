68897bd7928170393308dbddd5e8e5b746499fdc
Kenny Tran <82717216+ktra99@users.noreply.github.com>
2023-05-06 10:48:44 +0200

    change public profile button

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -572,7 +572,7 @@ export default function Account() {
                           <div className="mt-6 flex flex-col justify-stretch space-y-4">
                             <Link
                               href={"/board/d/" + profile.data.id}
-                              className="inline-flex items-center justify-center space-x-1 rounded-md bg-brand-50 px-3 py-2 text-sm font-semibold text-brand-900 shadow-sm ring-1 ring-inset ring-brand-300 hover:bg-brand-50"
+                              className="inline-flex items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-semibold text-brand-50 shadow-sm hover:bg-brand-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-600"
                             >
                               Visit
                             </Link>
